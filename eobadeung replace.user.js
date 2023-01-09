03c22cb84e1fda576b926a5dd216a3b10652b9a2
chujuchuchu <122243266+chujuchuchu@users.noreply.github.com>
2023-01-08 20:00:06 -0800

    Add files via upload

---
+++ eobadeung replace.user.js
@@ -0,0 +1,410 @@
+// ==UserScript==
+// @name         eobadeung replace
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  RIDI CAN'T STOP ME
+// @author       chujuchuchu
+// @match        https://view.ridibooks.com/books/425*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=tampermonkey.net
+// @grant        none
+// ==/UserScript==
+
+(function() {
+
+
+    setTimeout(function() {
+
+
+        var namesreplace = names;
+        var totalnames = namesreplace.length;
+
+        var hangulnames = [];
+        hangulnames.length = namesreplace.length;
+
+        var romannames = [];
+        romannames.length = namesreplace.length;
+
+        for (let i = 0; i < namesreplace.length; i++) {
+            let splittext = namesreplace[i].split(",");
+            let regtext = new RegExp(splittext[0], "g");
+            hangulnames[i] = regtext;
+            romannames[i] = splittext[1];
+        }
+
+        var honorificsreplace = honorifics;
+        var totalhonorifics = honorificsreplace.length;
+
+        var hangulhonorifics = [];
+        hangulhonorifics.length = honorificsreplace.length;
+
+        var romanhonorifics = [];
+        romanhonorifics.length = honorificsreplace.length;
+
+        for (let i = 0; i < honorificsreplace.length; i++) {
+            let splittext = honorificsreplace[i].split(";");
+            let regtext = new RegExp(splittext[0], "g");
+            hangulhonorifics[i] = regtext;
+            romanhonorifics[i] = splittext[1];
+        }
+
+        var txtWalker = document.createTreeWalker (
+            document.body,
+            NodeFilter.SHOW_TEXT,
+            { acceptNode: function (node) {
+                if (node.nodeValue.trim() ) {
+                    return NodeFilter.FILTER_ACCEPT; }
+
+                return NodeFilter.FILTER_SKIP;
+            }
+            },
+            false
+        );
+
+        var txtNode = null;
+
+        while (txtNode = txtWalker.nextNode () ) {
+            var oldTxt = txtNode.nodeValue;
+
+            for (let i = 0; i < totalnames; i++) {
+                oldTxt = oldTxt.replaceAll(hangulnames[i], romannames[i]);
+            }
+            for (let i = 0; i < totalhonorifics; i++) {
+                oldTxt = oldTxt.replaceAll(hangulhonorifics[i], romanhonorifics[i]);
+            }
+            txtNode.nodeValue = oldTxt;
+        }
+
+
+    }, 2000);
+
+
+
+
+
+
+})();
+
+
+var names = ["수혁,Soohyuk",
+             "프리야 쿠마리,Priya Kumari",
+             "프리야,Priya",
+             "쿠마리,Kumari",
+             "마리스,MARIS",
+             "올리비에 마틴,Olivier Martin",
+             "올리비에,Olivier",
+             "마틴,Martin",
+             "박무현,Park Moohyun",
+             "무현,Moohyun",
+             "네바에 홉킨스,Neva Hopkins",
+             "네바에,Neva",
+             "홉킨스,Hopkins",
+             "에밀리,Emily",
+             "엘리엇 브라운,Elliot Brown",
+             "엘리엇,Elliot",
+             "이빈,Lee Bin",
+             "왕웨이,Wang Wei",
+             "제임스 밀러,James Miller",
+             "제임스,James",
+             "밀러,Miller",
+             "카를로스 루이즈,Carlos Ruiz",
+             "루이즈,Carlos Ruiz",
+             "카를로스,Ruiz",
+             "투마나코 오란가,Tumanako Oranga",
+             "투마나코,Tumanako",
+             "오란가,Oranga",
+             "후미코,Fumiko",
+             "헨리,Henry",
+             "마이클 로아커,Michael Roarker",
+             "마이클,Michael",
+             "로아커,Roarker",
+             "마커스,Marcus",
+             "조동희,Cho Donghee",
+             "신해량,Shin Haeryang",
+             "씬해량,Sshin Haeryang",
+             "해량,Haeryang",
+             "강수정,Kang Soojung",
+             "수정,Soojung",
+             "김재희,Kim Jaehee",
+             "재희,Jaehee",
+             "백애영,Baek Aeyoung",
+             "애영,Aeyoung",
+             "서지혁,Seo Jihyuk",
+             "지혁,Jihyuk",
+             "정상현,Jung Sanghyun",
+             "상현,Sanghyun",
+             "사토 류스케,Sato Ryusuke",
+             "사토,Sato",
+             "류스케,Ryusuke",
+             "타마키 유즈루,Tamaki Yuzuru",
+             "타마키,Tamaki",
+             "유즈루,Yuzuru",
+             "타카하시 유리에,Takahashi Yurie",
+             "유리에,Yurie",
+             "타카하시,Takahashi",
+             "블라디미르 세르게예비치 이바노프,Vladimir Sergeevich Ivanov",
+             "블라디미르,Vladimir",
+             "세르게예비치,Sergeevich",
+             "이바노프,Ivanov",
+             "니키타,Nikita",
+             "니콜라이,Nikolai",
+             "드미트리,Dmitry",
+             "빅토르 바실예프,Victor Vasilyev",
+             "빅토르,Victor",
+             "바실예프,Vasilyev",
+             "소피야,Sophia",
+             "이리나,Irina",
+             "하이윤,Hai Yun",
+             "리웨이,Li Wei",
+             "슈란,Shu Ran",
+             "웨이치,Wei Qi",
+             "쯔쉬안,Zi Xuan",
+             "하오란,Hao Lan",
+             "홍타오,Hong Tao",
+             "데이비드,David",
+             "제니퍼 스미스,Jennifer Smith",
+             "스미스,Smith",
+             "제니퍼,Jennifer",
+             "대니얼 로드리게스,Daniel Rodriguez",
+             "대니얼,Daniel",
+             "로드리게스,Rodriguez",
+             "레너드 샌더스,Leonard Sanders",
+             "레너드,Leonard",
+             "샌더스,Sanders",
+             "케빈 윌슨,Kevin Wilson",
+             "케빈,Kevin",
+             "윌슨,Wilson",
+             "리암,Liam",
+             "해리 우즈,Harry Woods",
+             "해리,Harry",
+             "우즈,Woods",
+             "릴리,Lily",
+             "빅토리아,Victoria",
+             "카밀라,Camilla",
+             "타일러 존스,Tyler Jones",
+             "타일러,Tyler",
+             "존스,Jones",
+             "윌리엄 에릭슨,William Erikson",
+             "윌리엄,William",
+             "에릭슨,Erikson",
+             "안젤라 말론,Angela Malone",
+             "안젤라,Angela",
+             "유금이,Yoo Geumyi",
+             "금이씨,Geumyi-ssi",
+             "금이 씨,Geumyi-ssi",
+             "금이님,Geumyi-ssi",
+             "금이 님,Geumyi-ssi",
+             "금이야,Geumyi-ya",
+             "케빈 로이,Kevin Roy",
+             "더스틴,Dustin",
+             "로건,Logan",
+             "루카스,Lucas",
+             "마르티나 산체스,Martina Sanchez",
+             "마르티나,Martina",
+             "산체스,Sanchez",
+             "벤자민,Benjamin",
+             "샘 영,Sam Young",
+             "엠마,Emma",
+             "은자디,Njadi",
+             "은라디,Nradi",
+             "카누,Kano",
+             "장우이,Jang Wooyi",
+             "제임스 다넬,James Darnell",
+             "제임스,James",
+             "다넬,Darnell",
+             "벨리알 라일리,Belial Riley",
+             "벨리알,Belial",
+             "라일리,Riley",
+             "김가영,Kim Gayoung",
+             "가영,Gayoung",
+             "(\w)Gayoung,$1가영",
+             "아서 굿맨,Arthur Goodman",
+             "굿맨,Goodman",
+             "카터,Carter",
+             "무한Infinity교,Ininityism",
+             "무한교,Infinityism",
+             "마이클 로아커,Michael Roarker",
+             "마이클,Michael",
+             "로아커,Roarker",
+             "엘리자베스 위버,Elizabeth Weaver",
+             "엘리자베스,Elizabeth",
+             "카누,Kano",
+             "데이비드 나이트,David Knight",
+             "데이비드,David",
+             "나이트,Knight",
+             "파멜라 브라운,Pamela Brown",
+             "파멜라,Pamela",
+             "브라운,Brown",
+             "타일러 존스,Tyler Jones",
+             "타일러,Tyler",
+             "존스,Jones",
+             "러시아,Russia",
+             "가팀,Ga-team",
+             "나팀,Na-team",
+             "다팀,Da-team",
+             "라팀,Ra-team",
+             "마팀,Ma-team",
+             "바팀,Ba-team",
+             "사팀,Sa-team",
+             "아팀,Ah-team",
+             "해저기지,Undersea Station",
+             "대한도,Daehan Island",
+             "중앙동,Center Building",
+             "청룡동,Cheongryong Building",
+             "백호동,Baekho Building",
+             "주적동,Joojeok Building",
+             "현무동,Hyunmoo Building",
+             "미스터 팍,Mr. Park",
+             "미스터 씬,Mr. Shin",
+             "이지현,Lee Jihyun",
+             "지현,Jihyun",
+             "신팀장,Team Leader Shin",
+             "신 팀장,Team Leader Shin",
+             "강부팀장,Deputy Chief Kang",
+             "강 부팀장,Deputy Chief Kang",
+             "(\w)Kano,\]1카누",
+             "(\w)Geumyi,$1금이",
+             "(\w)Haeryang,$1해량",
+             "(\w)Soojung,$1수정",
+             "(\w)Yurie,$1유리에",
+             "(\w)Jihyun,$1지현",
+             "우에하라 스미레,Uehara Sumire",
+             "스미레,Sumire",
+             "우에하라,Uehara",
+             "야마시타,Yamashita",
+             "이치다,Ichita",
+             "스즈키,Suzuki",
+             "치과 선생님,dentist seongsaeng-nim",
+             "미첼이라,Mitchell",
+             "조셉 그레이엄,Joseph Graham",
+             "조셉,Joseph",
+             "위버,Weaver",
+             "베스,Beth",
+             "리즈,Liz",
+             "캐새키,Kaesaeki"
+                   ];
+
+var honorifics = ["([a-z])이 형;$1-ie hyung",
+                  "([a-z])이 누나; $1-ie noona",
+                  "([a-z])이 오빠;$1-ie oppa",
+                  "([a-z]) 이 형;$1-ie hyung",
+                  "([a-z]) 이 누나; $1-ie noona",
+                  "([a-z])언니;$1 unnie",
+                  "([a-z])누님;$1 noonim",
+                  "([a-z])누나;$1 noona",
+                  "([a-z])형;$1 hyung",
+                  "([a-z])형님;$1 hyung-nim",
+                  "([a-z]) 언니;$1 unnie",
+                  "([a-z]) 언님;$1 unnim",
+                  "([a-z]) 누님;$1 noonim",
+                  "([a-z]) 누나;$1 noona",
+                  "([a-z]) 형님;$1 hyung-nim",
+                  "([a-z]) 형;$1 hyung",
+                  "([a-z])오라버니;$1 orabeoni",
+                  "([a-z])선배님;$1 sunbae-nim",
+                  "([a-z])후배님;$1 hoobae-nim",
+                  "([a-z]) 동생;$1 dongsaeng",
+                  "([a-z])동생;$1 dongsaeng",
+                  "([a-z]) 오라버니;$1 orabeoni",
+                  "([a-z]) 선배님;$1 sunbae-nim",
+                  "([a-z]) 후배님;$1 hoobae-nim",
+                  "([a-z]) 아저씨;$1 ahjussi",
+                  "([a-z])아저씨;$1 ahjussi",
+                  "([a-z])오빠;$1 oppa",
+                  "([a-z]) 오빠;$1 oppa",
+                  "([a-z]) 선배;$1 sunbae",
+                  "([a-z])선배;$1 sunbae",
+                  "([a-z])아;$1-ah",
+                  "([a-z])야;$1-ya",
+                  "([a-z]) 씨;$1-ssi",
+                  "([a-z])씨;$1-ssi",
+                  "([a-z]) 님;$1-nim",
+                  "([a-z])님;$1-nim",
+                  "([a-z]) 군;$1-gun",
+                  "([a-z]) 양;$1-yang",
+                  "형님;Hyung-nim",
+                  "오라버니;Orabeoni",
+                  "선배님;Sunbae-nim",
+                  "후배님;Hoobae-nim",
+                  "여동생;sister dongsaeng",
+                  "남동생;brother dongsaeng",
+                  "동생;dongsaeng",
+                  "X발;시발",
+                  "시X;시발",
+                  "X나;존나",
+                  "존X;존나",
+                  "X도;좆도",
+                  "좆X;좆도",
+                  "X새끼;씹새끼",
+                  "개X끼;개새끼",
+                  "X같;좆같",
+                  "언니;Unnie",
+                  "언님;Unnim",
+                  "누님;Noonim",
+                  "누나;Noona",
+                  "오빠;Oppa",
+                  "막내야;Maknae-ya",
+                  "\r\n막내;\r\nMaknae",
+                  "막내;maknae",
+                  "피디님;PD-nim",
+                  "([a-z]) 선생님; $1 seonsaeng-nim",
+                  "선생님; Seonsaeng-nim",
+                  "아저씨;Ahjussi",
+                  "PD님;PD-nim",
+                  "혀엉;Hyuung",
+                  "선배;sunbae",
+                  "도련님;Young Master-nim",
+                  "([[:punct:]])형;$1Hyung",
+                  "형([[:punct:]]);hyung$1",
+                  ", 형;, hyung",
+                  "\r\n형([[:space:]]);\r\nHyung$1",
+                  "\r\n형이;\r\nHyung이",
+                  "([[:space:]])형([[:space:]]);$1hyung$1",
+                  "([[:punct:]]) 형;$1 Hyung",
+                  "형이;hyung이",
+                  "형은;hyung은",
+                  "형도;hyung도",
+                  "형과;hyung과",
+                  "형입;hyung입",
+                  "형에게;hyung에게",
+                  "형으로;hyung으로",
+                  "형\r\n;hyung\r\n",
+                  "형의;hyung의",
+                  "형을;hyung을",
+                  "형들;hyung들",
+                  "형은;hyung은",
+                  "형만;hyung만",
+                  "혀어어어엉;hyuuung",
+                  "\r\n형은;\r\nhyung은",
+                  "hyung([^a-z -.,…“\"이에의을과?‘'’은들도아]);형$1",
+                  "Hyung([^a-z -.,…“\"이에의을과?‘'’은들도아]);형$1",
+                  "([^a-zA-Z “\"‘-])hyung;$1형",
+                  "([^a-zA-Z “\"‘-])Noona;$1누나",
+                  "([a-z]) Ahjussi; $1 ahjussi",
+                  "([a-z]) Noona; $1 noona",
+                  "형한테;hyung한테",
+                  "대표님;Daepyo-nim",
+                  "(\w)sunbae;$1선배",
+                  "(\w)Sunbae;$1선배",
+                  "년 hyung이;년 형이",
+                  "하이고오;haigooo",
+                  "아이고오;aigooo",
+                  "하이고;haigoo",
+                  "아이고;aigoo",
+                  "([[:space:]])([[:space:]])-yang;$1양",
+                  "ah-ah;aah",
+                  "([aeiouhy])-ah;$1아",
+                  "([^aeiouhy])-ya;$1야",
+                  "작은형;Younger hyung",
+                  "큰형;Older hyung",
+                  "‘님’;‘nim’",
+                  "우와 씨;Uwah ssi",
+                  "씨이바아알;FuUuCk",
+                  "에이 씨;Eyy ssi",
+                  "아이 씨;Aiy ssi",
+                  "―형~;―Hyung~"
+];
+
+
+
+
+
---
+++ jeolsildol replace.user.js
@@ -0,0 +1,342 @@
+// ==UserScript==
+// @name         jeolsildol replace
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  RIDI CAN'T STOP ME
+// @author       chujuchuchu
+// @match        https://view.ridibooks.com/books/205*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=tampermonkey.net
+// @grant        none
+// ==/UserScript==
+
+(function() {
+
+
+    setTimeout(function() {
+
+
+        var namesreplace = names;
+        var totalnames = namesreplace.length;
+
+        var hangulnames = [];
+        hangulnames.length = namesreplace.length;
+
+        var romannames = [];
+        romannames.length = namesreplace.length;
+
+        for (let i = 0; i < namesreplace.length; i++) {
+            let splittext = namesreplace[i].split(",");
+            let regtext = new RegExp(splittext[0], "g");
+            hangulnames[i] = regtext;
+            romannames[i] = splittext[1];
+        }
+
+        var txtWalker = document.createTreeWalker (
+            document.body,
+            NodeFilter.SHOW_TEXT,
+            { acceptNode: function (node) {
+                if (node.nodeValue.trim() ) {
+                    return NodeFilter.FILTER_ACCEPT; }
+
+                return NodeFilter.FILTER_SKIP;
+            }
+            },
+            false
+        );
+
+        var txtNode = null;
+
+        while (txtNode = txtWalker.nextNode () ) {
+            var oldTxt = txtNode.nodeValue;
+
+            for (let i = 0; i < totalnames; i++) {
+                oldTxt = oldTxt.replaceAll(hangulnames[i], romannames[i]);
+            }
+
+            txtNode.nodeValue = oldTxt;
+        }
+
+
+    }, 2000);
+
+
+
+
+
+
+})();
+
+
+var names = ["정해원,Jeong Haewon",
+             "민지호,Min Jiho",
+             "황새벽,Hwang Saebyeok",
+             "안주원,Ahn Joowon",
+             "한효석,Han Hyoseok",
+             "박선재,Park Seonjae",
+             "신지운,Shin Jiwoon",
+             "퍼스트라이트,First Light",
+             "스퀘어,Square",
+             "부정태,Boo Jeongtae",
+             "국민이 선택한 아이돌,People's Choice Idol",
+             "국선아,PCI",
+             "박종렬,Park Jongryeol",
+             "곽윤용,Kwak Yoonyong",
+             "박희영,Park Heeyoung",
+             "송강원,Song Kangwon",
+             "박유나,Park Yuna",
+             "장용규,Jang Yonggyu",
+             "정기훈,Jeong Kihoon",
+             "유선아,Yoo Seonah",
+             "이예영,Lee Yeyoung",
+             "철순,Cheolsoon",
+             "강하연,Kang Hayeon",
+             "박희택,Park Heetaek",
+             "박중운,Park Joongwoon",
+             "양이형,Yang Yihyung",
+             "박선혜,Park Seonhye",
+             "장지영,Jang Jiyoung",
+             "민조,Minjo",
+             "퍼라,FiLi",
+             "정선미,Jeong Seonmi",
+             "강영호,Kang Youngho",
+             "김재성,Kim Jaeseong",
+             "안쭈,Anjju",
+             "정수연,Jeong Sooyeon",
+             "우하정,Woo Hajeong",
+             "효식,Hyosik",
+             "해원,Haewon",
+             "지호,Jiho",
+             "주원,Joowon",
+             "효석,Hyoseok",
+             "선재,Seonjae",
+             "지운,Jiwoon",
+             "정태,Jeongtae",
+             "종렬,Jongryeol",
+             "윤용,Yoonyong",
+             "희영,Heeyoung",
+             "강원,Kangwon",
+             "유나,Yuna",
+             "용규,Yonggyu",
+             "기훈,Kihoon",
+             "선아,Seonah",
+             "예영,Yeyoung",
+             "하연,Hayeon",
+             "희택,Heetaek",
+             "중운,Joongwoon",
+             "이형,Yihyung",
+             "선혜,Seonhye",
+             "지영,Jiyoung",
+             "선미,Seonmi",
+             "영호,Youngho",
+             "재성,Jaeseong",
+             "수연,Sooyeon",
+             "하정,Hajeong",
+             "새벽 :,Saebyeok :",
+             "새벽이 형,Saebyeok-ie hyung",
+             "새벽아,Saebyeok-ah",
+             "새벽이,Saebyeok이",
+             "새벽씨,Saebyeok-ssi",
+             "새벽 씨,Saebyeok-ssi",
+             "새벽 군,Saebyeok-gun",
+             "새벽 형,Saebyeok hyung",
+             "새벽의,Saebyeok의",
+             "새부기,Saeboogi",
+             "리더부기,Leader Boogi",
+             "강한우,Kang Hanwoo",
+             "앤서니 맥긴리,Anthony McKinley",
+             "앤서니,Anthony",
+             "맥긴리,McKinley",
+             "신희범,Shin Heebeom",
+             "박경석,Park Kyungseok",
+             "빅 블루,Big Blue",
+             "박건훈,Park Geonhoon",
+             "장석훈,Jang Seokhoon",
+             "X버스,Xverse",
+             "황새,Hwangsae",
+             "이승주,Lee Seungjoo",
+             "김주철,Kim Joocheol",
+             "햇살,Haetsal",
+             "국혐,National Hated",
+             "이재희,Lee Jaehee",
+             "박재원,Park Jaewon",
+             "이희세,Lee Heese",
+             "강윤석,Kang Yoonseok",
+             "아이노,INO",
+             "올리버,Oliver",
+             "이수한,Lee Soohan",
+             "배드원,Bad One",
+             "뉴데이즈,NEW days",
+             "박상현,Park Sanghyun",
+             "최재빈,Choi Jaebin",
+             "김성민,Kim Seongmin",
+             "앤써즈,Answers",
+             "아이라이트,ILIGHT",
+             "최정호,Choi Jeongho",
+             "곽준우,Kwak Joonwoo",
+             "앤 서방,An-seobang",
+             "강진기,Kang Jinki",
+             "강효준,Kang Hyojoon",
+             "희범,Heebeok",
+             "건훈,Kyungseok",
+             "석훈,Geonhoon",
+             "승주,Seokhoon",
+             "승주,Seungjoo",
+             "주철,Joocheol",
+             "재희,Jaehee",
+             "재원,Jaewon",
+             "희세,Heese",
+             "윤석,Yoonseok",
+             "수한,Soohan",
+             "상현,Sanghyun",
+             "재빈,Jaebin",
+             "성민,Seongmin",
+             "정호,Jeongho",
+             "준우,Joonwoo",
+             "진기,Jinki",
+             "효준,Hyojoon",
+             "카일룸,KYLUM",
+             "차우석,Cha Wooseok",
+             "우석,Wooseok",
+             "쭈 어머님,Jjumom",
+             "지우니,Jiwooni",
+             "리더님,Leader-nim",
+             "쭈워니,Juuwoni",
+             "황수정 과장님,Manager Hwang Soojung",
+             "황수정,Hwang Soojung",
+             "최정민,Choi Jungmin",
+             "유찬희,Yoo Chanhee",
+             "다니엘 서,Daniel Seo",
+             "다니엘,Daniel",
+             "다니 형,Dani hyung",
+             "희은,Heeeun",
+             "강진영,Kang Jinyung",
+             "배신자2,Traitor 2",
+             "스파이1,Spy 1",
+             "최기문,Choi Kimoon",
+             "세미님,Semi-nim",
+             "세미 씨,Semi-ssi",
+             "서민혁,Seo Minhyuk",
+             "채연재,Chae Yunjae",
+             "대장님,Captain",
+             "Minjo조,Minjojo",
+             "Hyosik이 :,Hyosik-ie :",
+             "배신자1,Traitor 1",
+             "스파이2,Spy 2",
+             "정민,Jungmin",
+             "찬희,Chanhee",
+             "진영,Jinyung",
+             "기문,Kimoon",
+             "민혁,Minhyuk",
+             "연재,Yunjae",
+             "로건 윤혁 최,Logan Yoonhyuk Choi",
+             "로건,Logan",
+             "슬Logan,슬로건",
+             "백민형,Baek Minhyung",
+             "실트,trending",
+             "채유호,Chae Yooho",
+             "우혜정,Woo Hyejeong",
+             "황지석,Hwang Jiseok",
+             "루나리스,Lunaris ",
+             "마태오,Matthaios ",
+             "최윤솔,Choi Yoonsol",
+             "소년들,Boys ",
+             "박수현,Park Soohyun",
+             "유호,Yooho",
+             "혜정,Hyejeong",
+             "지석,Jiseok",
+             "윤솔,Yoonsol",
+             "수현,Soohyun",
+             "에블린 노을 맥긴리,Evelyn Noeul McKinley",
+             "에블린,Evelyn",
+             "에블린 노을 McKinley,Evelyn Noeul McKinley",
+             "철아,Cheol-ah",
+             "배수진,Bae Soojin",
+             "박건후,Park Gunhoo",
+             "허해준,Heo Haejun",
+             "황지훈,Hwang Jihoon",
+             "지훈이,Jihoon이",
+             "이준희,Lee Junhee",
+             "준희,Junhee",
+             "박민하,Park Minha",
+             "심한철,Shim Hancheol",
+             "강여은,Kang Yeoeun",
+             "(\w)Soohan,$1수한",
+             "데이브 레비탄,Dave Levitan",
+             "티케,Tike",
+             "티케 엔터테인먼트,Tike Entertainment",
+             "이용민,Lee Yongmin",
+             "강채은,Kang Chaeeun",
+             "강현서,Kang Hyunseo",
+             "강대형,Kang Daehyung",
+             "박호현,Park Hohyun",
+             "호현,Hohyun",
+             "김수경,Kim Sookyung",
+             "수경,Sookyung",
+             "이정석,Lee Jungsuk",
+             "지웅이,Ji Woongi",
+             "최동국,Choi Dongguk",
+             "이춘형,Lee Chunhyung",
+             "임수환,Im Soohwan",
+             "쭈원,Jjuwon",
+             "쭈워나,Jjuwonah",
+             "우정훈,Woo Jonghoon",
+             "이원일,Lee Wonil",
+             "원일,Wonil",
+             "(\w)Wonil,$1원일",
+             "이선겸,Lee Seongyeom",
+             "선겸,Seongyeom",
+             "강유안,Kang Yooan",
+             "유안,Yooan",
+             "도윤,Doyun",
+             "강한영,Kang Hanyoung",
+             "곽민재,Kwak Minjae",
+             "민재,Minjae",
+             "새북아,Saeboog-ah",
+             "보이드 엔터테인먼트,VOID Entertainment",
+             "보이드 엔터,VOID Enter",
+             "보이드,VOID",
+             "김문재,Kim Moonjae",
+             "문재,Moonjae",
+             "박해린,Park Haerin",
+             "정 PD,PD Jeong",
+             "온 피디,PD Ohn",
+             "곽신희,Kwak Shinhee",
+             "윤시연,Yoon Siyeon",
+             "클라루스,CLARUS",
+             "송다온,Song Daon",
+             "브삼,BEUSAM",
+             "Heeyoung이 Noona,Heeyoung-ie noona",
+             "박재우,Park Jaewoo",
+             "박해린,Park Haerin",
+             "해린,Haerin",
+             "효시깅,Hyoshiking",
+             "주수린,Joo Soorin",
+             "박유미,Park Yoomi",
+             "박상우,Park Sangwoo",
+             "주니 형,Juni hyung",
+             "매hyung,매형",
+             "노을,Noeul",
+             "김준서,Kim Junseo",
+             "박의신,Park Euishin",
+             "의신,Euishin",
+             "데이브 유,Dave Yoo",
+             "데이브,Dave",
+             "뉴데,New Day",
+             "송다온,Song Daon",
+             "다온,Daon",
+             "Yeyoung이 Noona,Yeoyoung-ie noona",
+             "고석희,Koh Seokhee",
+             "진형,Jinhyung",
+             "백범준,Baek Beomjun",
+             "범준,Beomjun-ie hyung",
+             "정 씨,Jeong-ssi",
+             "무로,Muro",
+             "(\w)Muro,$1무로",
+             "강 씨,Kang-ssi",
+             "신비은,Shin Bieun",
+             "비은,Bieun",
+             "(\w)Bieun,$1비은",
+             "명소은,Myung Soeun",
+             "소은,Soeun",
+             "(\w)Soeun,$1소은"
+                   ];
